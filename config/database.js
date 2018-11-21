c7c93306d6605354fdbd9eb4a08b11f26212877e
abdullah1111 <abdullah.ansari587@gmail.com>
2018-11-22 00:28:49 +0500

    Add files via upload

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 //Set up mongoose connection
 const mongoose = require('mongoose');
-const mongoDB = 'mongodb://localhost/node_rest_api';
+const mongoDB = 'mongodb://abd:1234@cluster0-shard-00-00-j3tem.mongodb.net:27017,cluster0-shard-00-01-j3tem.mongodb.net:27017,cluster0-shard-00-02-j3tem.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true';
 mongoose.connect(mongoDB);
 mongoose.Promise = global.Promise;
 module.exports = mongoose;
